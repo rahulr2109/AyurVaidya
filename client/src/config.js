22dcab5d6c3d78e924ceca576edd9e908769c50a
iamthehr <harshraj5565@gmail.com>
2023-12-20 14:17:43 +0530

    deployed

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "http://localhost:4000/";
+let BASE_URL = "https://ayurvaidya.onrender.com/";
 let new_URL = "https://quotabackendnew.onrender.com/";
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 //   BASE_URL = "http://localhost:4000/";

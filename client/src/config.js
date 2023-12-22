c51c70166c187641c7d99fbd9ce7ffd807ba28e0
Aditya5510 <78204626+Aditya5510@users.noreply.github.com>
2023-12-22 18:54:02 +0530

    reverted

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
-// let YBASE_URL = "https://ayurvaidya.onrender.com/";
-// let new_URL = "https://quotabackendnew.onrender.com/";
-let BASE_URL = "https://ayur-vaidya.vercel.app/";
+let YBASE_URL = "https://ayurvaidya.onrender.com/";
+let new_URL = "https://quotabackendnew.onrender.com/";
+let BASE_URL = "http://localhost:4000/";
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 //   BASE_URL = "http://localhost:4000/";
 // }

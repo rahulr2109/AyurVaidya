3b719ecb664f015dfc5cb0782a007347d4a1eca0
Aditya5510 <78204626+Aditya5510@users.noreply.github.com>
2023-12-22 18:18:27 +0530

    newcomomit

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 let YBASE_URL = "https://ayurvaidya.onrender.com/";
 let new_URL = "https://quotabackendnew.onrender.com/";
-let BASE_URL = "http://localhost:4000/";
+let BASE_URL = "https://ayur-vaidya.vercel.app/";
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 //   BASE_URL = "http://localhost:4000/";
 // }
